677c5d3cf69fc1d4bee679cf8fa714d773bf7a15
Yves Atsuchi Takaoka <yvesat@gmail.com>
2023-11-12 21:05:04 -0300

    Update parking slot grid and grid tile to show vehicle and parking slot data

--- lib/src/view/widgets/parking_slot_grid.dart
+++ lib/src/view/widgets/parking_slot_grid.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:parking_manager/src/controller/parking_slot_controller.dart';
 
 import 'parking_slot_gridtile.dart';
 
@@ -8,14 +9,14 @@ class ParkingSlotGrid extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // ref.watch(filteredProductsProvider);
-    // final productList = ref.watch(filteredProductsProvider.notifier).filterdProductsList();
+    final parkingSlotState = ref.watch(parkingSlotControllerProvider.notifier).getParkingSlotState(ref);
 
     return GridView.builder(
       shrinkWrap: true,
-      itemCount: 18,
-      // itemCount: productList.length,
-      itemBuilder: (ctx, i) => const ParkingSlotGridTile(),
+      itemCount: parkingSlotState.length,
+      itemBuilder: (ctx, index) => ParkingSlotGridTile(
+        parkingslotNumber: parkingSlotState[index].parkingSlotNumber,
+      ),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         childAspectRatio: 3 / 2,
--- lib/src/view/widgets/parking_slot_gridtile.dart
+++ lib/src/view/widgets/parking_slot_gridtile.dart
@@ -1,40 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:parking_manager/src/controller/parking_slot_controller.dart';
+import 'package:parking_manager/src/controller/vehicle_controller.dart';
+
+import '../pages/parking_slot_control_page.dart';
 
 class ParkingSlotGridTile extends ConsumerWidget {
-  // final String id;
-  const ParkingSlotGridTile({super.key});
-  // const ParkingSlotTile({required this.id, super.key});
+  final int parkingslotNumber;
+  const ParkingSlotGridTile({required this.parkingslotNumber, super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final parkingSlotController = ref.read(parkingSlotControllerProvider.notifier);
+    final parkingSlotState = parkingSlotController.getParkingSlot(ref, parkingslotNumber);
+
+    final vehicleController = ref.read(vehicleControllerProvider.notifier);
+    final vehicleState = vehicleController.getVehicle(ref, parkingSlotState!.occupyingVehicleId);
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
-      child: const Card(
-        elevation: 1.5,
-        child: Padding(
-          padding: EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  // mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    FaIcon(FontAwesomeIcons.carSide, size: 30, color: Colors.green),
-                    SizedBox(width: 12),
-                    Text(
-                      "Vaga X",
-                      style: TextStyle(fontWeight: FontWeight.bold),
+      child: InkWell(
+        onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ParkingSlotControlPage(parkingslotNumber)),
+          // onTap: () => context.go(
+          //   Uri(
+          //     path: 'parkingSlotControl',
+          //     queryParameters: {'parkingslotNumber': parkingslotNumber.toString()},
+          //   ).toString(),
+        ),
+        child: Card(
+          elevation: 1.5,
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      FaIcon(FontAwesomeIcons.carSide, size: 30, color: parkingSlotState.available ? Colors.green : Colors.red),
+                      const SizedBox(width: 12),
+                      Text(
+                        "Vaga ${parkingSlotState.parkingSlotNumber}",
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  if (!parkingSlotState.available)
+                    SizedBox(
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 8),
+                          Text("${vehicleState!.brand} ${vehicleState.model}"),
+                          const SizedBox(height: 4),
+                          Text(vehicleState.licensePlate),
+                        ],
+                      ),
                     ),
-                  ],
-                ),
-                SizedBox(height: 8),
-                Text("Volkswagen Gol"),
-                SizedBox(height: 4),
-                Text("ABC - 1234"),
-              ],
+                  if (parkingSlotState.available)
+                    const Padding(
+                      padding: EdgeInsets.only(top: 16),
+                      child: Text(
+                        "Vaga disponível!",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                ],
+              ),
             ),
           ),
         ),
