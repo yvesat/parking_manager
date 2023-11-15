fec4845426f3fac775c2380e31bfa2fdae9e7fcc
Yves Atsuchi Takaoka <yvesat@gmail.com>
2023-11-15 19:35:33 -0300

    Create entry and exit date picker

--- lib/src/view/pages/parking_slot_control_page.dart
+++ lib/src/view/pages/parking_slot_control_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 
 import '../../controller/parking_slot_controller.dart';
 import '../../controller/vehicle_controller.dart';
@@ -19,6 +20,8 @@ class ParkingSlotControlPage extends ConsumerStatefulWidget {
 }
 
 class _ParkingSlotControlPageState extends ConsumerState<ParkingSlotControlPage> {
+  DateTime? _entryDate = DateTime.now();
+  DateTime _exitDate = DateTime.now();
   String _displayStringForOption(VehicleModel option) => "${option.brand} ${option.model} - ${option.licensePlate}";
   int? _selectedVehicleId;
 
@@ -28,9 +31,14 @@ class _ParkingSlotControlPageState extends ConsumerState<ParkingSlotControlPage>
 
     final parkingSlotController = ref.read(parkingSlotControllerProvider.notifier);
     final parkingSlotState = parkingSlotController.getParkingSlot(ref, context, widget.parkingSlotNumber);
+
     final vehicleController = ref.watch(vehicleControllerProvider.notifier);
     final vehicleState = vehicleController.getVehicleState(ref);
     final vehicle = vehicleController.getVehicle(ref, parkingSlotState!.occupyingVehicleId);
+
+    final parkingRecord = parkingSlotController.getParkingRecordByParkSLNUM(ref, parkingSlotState.parkingSlotNumber);
+
+    if (parkingRecord != null) _entryDate = parkingRecord.entryDate;
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -66,7 +74,7 @@ class _ParkingSlotControlPageState extends ConsumerState<ParkingSlotControlPage>
                           return const Iterable<VehicleModel>.empty();
                         }
                         return vehicleState.where((VehicleModel option) {
-                          return option.licensePlate.contains(textEditingValue.text.toLowerCase());
+                          return option.licensePlate.contains(textEditingValue.text.toUpperCase());
                         });
                       },
                       onSelected: (VehicleModel selection) async {
@@ -91,6 +99,79 @@ class _ParkingSlotControlPageState extends ConsumerState<ParkingSlotControlPage>
                 ),
               ),
             VehicleDetailCard(vehicleId: _selectedVehicleId ?? vehicle?.vehicleId),
+            InkWell(
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Data da Entrada: ",
+                              style: TextStyle(fontSize: 12),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              DateFormat('dd/MM/yyyy').format(_entryDate!),
+                              style: const TextStyle(fontSize: 14),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Icon(Icons.calendar_today, color: Theme.of(context).colorScheme.primary),
+                    ],
+                  ),
+                ),
+              ),
+              onTap: () async {
+                final newDate = await parkingSlotController.setDate(context, ref, _entryDate!);
+                if (newDate != null) {
+                  setState(() {
+                    _entryDate = newDate;
+                  });
+                }
+              },
+            ),
+            if (!parkingSlotState.available)
+              InkWell(
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "Data da Saída: ",
+                                style: TextStyle(fontSize: 12),
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                DateFormat('dd/MM/yyyy').format(_exitDate),
+                                style: const TextStyle(fontSize: 14),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Icon(Icons.calendar_today, color: Theme.of(context).colorScheme.primary),
+                      ],
+                    ),
+                  ),
+                ),
+                onTap: () async {
+                  final newDate = await parkingSlotController.setDate(context, ref, _exitDate);
+                  if (newDate != null) {
+                    setState(() {
+                      _exitDate = newDate;
+                    });
+                  }
+                },
+              ),
             const SizedBox(height: 20),
             ElevatedButton(
               //Habilita botão caso o usuário já tenha selecionado um veículo pelo autocomplete ou
@@ -101,7 +182,7 @@ class _ParkingSlotControlPageState extends ConsumerState<ParkingSlotControlPage>
                       if (parkingSlotState.available) {
                         final vehicleParkedAtSlot = parkingSlotController.isVehiceParked(ref, _selectedVehicleId!);
                         if (vehicleParkedAtSlot == null) {
-                          await parkingSlotController.setVehicleEntry(ref, context, _selectedVehicleId, parkingSlotState.parkingSlotNumber);
+                          await parkingSlotController.setVehicleEntry(ref, context, _selectedVehicleId!, parkingSlotState.parkingSlotNumber, _entryDate!);
                           if (context.mounted) {
                             alert.snack(context, "Veículo alocado a vaga ${widget.parkingSlotNumber}.");
                             context.pop();
